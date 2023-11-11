3673f394df790522d66215560528cb02f3508762
Thai Ha <halequocthai@outlook.com>
2023-11-11 09:25:11 +0700

    Add new routes
    GET /api/user/all
    PUT /api/user/role
    PUT /api/user/name

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -60,3 +60,66 @@ export const login = async (req, res) => {
         return res.status(500).json(e.message);
     }
 }
+
+export const changeRole = async (req, res) => {
+    try {
+        if (!req.body.id || !req.body.role) {
+            return res.status(400).json({
+                message: 'Missing input parameter!'
+            })
+        }
+        const findUser = await userModel.findById(req.body.id);
+        if (!findUser) {
+            return res.status(400).json({
+                message: 'User does not exist!'
+            })
+        }
+        findUser.role = req.body.role;
+        await findUser.save();
+        return res.status(200).json({
+            message: 'Change role successful!'
+        })
+    } catch (e) {
+        return res.status(500).json(e.message);
+    }
+}
+
+export const allUsers = async (req, res) => {
+    try {
+        const users = await userModel.find();
+        return res.status(200).json([
+            ...users.map(user => ({
+                id: user._id,
+                email: user.email,
+                name: user.name,
+                role: user.role
+            }))
+        ])
+    } catch (e) {
+        return res.status(500).json(e.message);
+    }
+}
+
+export const changeName = async (req, res) => {
+    try {
+        if (!req.body.id || !req.body.name) {
+            return res.status(400).json({
+                message: 'Missing input parameter!'
+            })
+        }
+        const findUser = await userModel.findById(req.body.id);
+        if (!findUser) {
+            return res.status(400).json({
+                message: 'User does not exist!'
+            })
+        }
+        findUser.name = req.body.name;
+        await findUser.save();
+        return res.status(200).json({
+            message: 'Change user information successful!'
+        })
+    }
+    catch {
+        return res.status(500).json(e.message);
+    }
+}
